a73b5f3774135b6df126f9c5c35c8c685c97d6be
System Administrator <kevinqi34>
2016-05-16 12:55:43 -0400

    update

--- php/oauth2callback.php
+++ php/oauth2callback.php
@@ -4,16 +4,16 @@ require_once '../google-api-php-client/src/Google/autoload.php';
 // Start a session to persist credentials.
 session_start();
 
-echo "hello";
+
 
 // Create the client object and set the authorization configuration
 // from the client_secrets.json you downloaded from the Developers Console.
 $client = new Google_Client();
-$client->setAuthConfigFile('client_secrets.json');
+$client->setAuthConfigFile('../kqi_data_key.json');
 $client->setRedirectUri('http://' . $_SERVER['HTTP_HOST'] . '/oauth2callback.php');
 $client->addScope(Google_Service_Analytics::ANALYTICS_READONLY);
 
-// Handle authorization flow from the server.
+// Handle authorization flow from the server.s
 if (! isset($_GET['code'])) {
   $auth_url = $client->createAuthUrl();
   header('Location: ' . filter_var($auth_url, FILTER_SANITIZE_URL));
