700013d9f1d928353514eea283c7d9570dea1228
System Administrator <kevinqi34>
2016-05-16 20:00:34 -0400

    update

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE HTML>
 <html>
-<?php include("./php/get_analytics_data.php") ?>
+
 <head>
 <!-- Signature -->
 <script>
@@ -108,7 +108,7 @@ Have skills? Contact ME!
 
 <div id="about">
 
-
+<?php include("./php/get_analytics_data.php") ?>
 
 
 
