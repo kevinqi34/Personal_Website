9d292152d75a6423525ee3373c66fa96c08db7c4
System Administrator <kevinqi34>
2016-05-18 17:16:44 -0400

    update

--- js/data_graph.js
+++ js/data_graph.js
@@ -1,20 +1,18 @@
 
 function createGraph (input) {
 
+  setTimeout(function () {
 
-  g = new Dygraph(
-      document.getElementById(input),
-      "../analytics_data_files/data.csv", // path to CSV file
-      {
-        
-
-      }
-    );
+    g = new Dygraph(
+        document.getElementById(input),
+        "../analytics_data_files/data.csv", // path to CSV file
+        {
 
 
+        }
+      );
 
-
-
+    }, 5000);
 
 
 
