af14493ab415ab406e86f376908497469265c3d0
System Administrator <kevinqi34>
2016-05-19 16:47:02 -0400

    update

--- php/bio.php
+++ php/bio.php
@@ -1,3 +1,3 @@
 <p>I'm an incoming freshman to the University of Michigan for CS.</p>
-<p>In my free time I play basketball, travel and find new, intresting cuisine to eat, but I also develop software.</p>
+<p>In my free time I play basketball, travel and find new interesting cuisine to eat. But I also develop software.</p>
 <p>You can find <b>live stats</b> for some of my previous projects below. <b>Enjoy!</b></p>
