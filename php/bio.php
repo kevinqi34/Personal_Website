2d324405364e534d00333916860b92229623c2b6
System Administrator <kevinqi34>
2016-05-19 14:28:18 -0400

    update

--- php/bio.php
+++ php/bio.php
@@ -1,3 +1,3 @@
 <p>I'm an incoming freshman to the University of Michigan for CS.</p>
 <p>In my free time I play basketball, travel and find new things to eat, but I also develop software.</p>
-<p>You can find live stats for some of my previous projects below. Enjoy!</p>
+<p>You can find <b>live stats</b> for some of my previous projects below. <b>Enjoy!</b></p>
