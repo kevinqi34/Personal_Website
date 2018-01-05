50ec16e4566f1c7bef1e28c73c1e33ee3e445e8f
kevinqi34 <sqi@novischools.net>
2018-01-04 19:44:58 -0500

    update

--- php/bio.php
+++ php/bio.php
@@ -1,2 +1,2 @@
-<p>I'm a freshman at the University of Michigan majoring in CS at the COE.</p>
+<p>I'm a student at the University of Michigan majoring in CS at the COE.</p>
 <p>In my free time I play basketball, travel and find new interesting cuisine to eat. But I also develop software.</p>
